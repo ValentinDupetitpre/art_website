a66984c0c89b65fd89e30dcbd00931ee11181b30
ValentinDupetitpre <valentin.dupetitpre@gmail.com>
2019-12-27 12:41:28 +0100

    information page for mobile

--- src/components/common/ArticlePreview.js
+++ src/components/common/ArticlePreview.js
@@ -1,9 +1,19 @@
-import React, {lazy, Suspense } from 'react'
+import React, {lazy, Suspense, useEffect } from 'react'
 import './ArticlePreview.css'
 
 const ImageComponent = lazy(() => import('./ImageComponent'));
 
 function ArticlePreview(props) {
+
+    useEffect(()=> {
+        const details = document.getElementsByClassName('article-details')
+        console.log(typeof(details))
+        for (let detail of details) { 
+            const lineheight = parseInt(window.getComputedStyle(detail).getPropertyValue("line-height"))
+            const lines=Math.floor(300/lineheight)
+            // detail.style['-webkit-line-clamp']=lines
+        }
+    })
 
     return(
         <div className="article-preview-wrapper">
@@ -16,7 +26,7 @@ function ArticlePreview(props) {
                 <div className="legend">
                     <span>{props.article ? props.article.type : ''}</span>
                     <h3>{props.article ? props.article.name : ''}</h3>
-                    <p>{props.article ? props.article.detail : ''}</p>
+                    <p className="article-details">{props.article ? props.article.detail : ''}</p>
                 </div>
             </div>
         </div>
